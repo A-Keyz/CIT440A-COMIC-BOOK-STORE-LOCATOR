c2b537a49b37124bfa05f2b1c2462a3e33f45bf4
criser115 <criser115@gmail.com>
2021-07-23 19:59:14 -0500

    Add project files.

---
+++ routes.py
@@ -0,0 +1,17 @@
+"""
+Routes and views for the bottle application.
+"""
+
+from bottle import route, view
+from datetime import datetime
+
+@route('/')
+@route('/home')
+@view('index')
+def home():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year
+    )
+
+
